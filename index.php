4ab0d91193e1cd725efd6d5483d7bef4b8a9e33f
PatrickNeff <patrick.neff@free.fr>
2015-05-16 13:10:09 +0200

    g merdé

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$db = new PDO("mysql:dbname=tp_commun;host=127.0.0.1", 'root', 'troiswa');
+// index.php
+// http://localhost/Site%20perso/PHTML/index.php?page=contact
+// $_GET['page'] -> contact
+// http://localhost/Site%20perso/PHTML/index.php?page=toto
+// $_GET['page'] -> toto
+// http://localhost/Site%20perso/PHTML/index.php?truc=bidule
+// $_GET['truc'] -> bidule
+// si $_GET['page'] = contact
+// charger le fichier apps/contact.php
+$page = 'home';
+if (isset($_GET['page']))
+	$page = $_GET['page'];
+require('views/index.phtml');
+?>
