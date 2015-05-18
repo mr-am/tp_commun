fd4867be62f2fe0f75ed62129a1a172b078df30e
bigcamo <nicolasguth67@gmail.com>
2015-05-18 15:28:29 +0200

    First upload / PB with chat loading

---
+++ apps/set-status.php
@@ -0,0 +1,22 @@
+<!-- inclusion de functions.php et connexion à la base de données MYSQL -->
+<?php
+session_start();
+include('functions.php');
+$db = db_connect();
+?>
+
+
+
+<!--  On ajoute une condition pour vérifier que le visiteur est connecté. 
+On ajoute ensuite une requête SQL pour modifier le statut de l'utilisateur. -->
+<?php
+if(user_verified()) {
+	$insert = $db->prepare('
+		UPDATE chat_online SET online_status = :status WHERE online_user = :user
+	');
+	$insert->execute(array(
+		'status' => $_POST['status'],
+		'user' => $_SESSION['id']		
+	));
+}
+?>
