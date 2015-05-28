ac009befe6b92f670b8160977d9faefaddf91923
PatrickNeff <patrick.neff@free.fr>
2015-05-28 10:30:14 +0200

    ajout mentions légales

--- apps/home.php
+++ apps/home.php
@@ -1,3 +1,7 @@
 <?php
-require('./views/home.phtml');
+//exécution de la requête:
+	$requete = "SELECT id, title, content, time_create, time_update, author_id FROM article ORDER BY id DESC";
+	$resultat = $db->query($requete);
+	$tableau_res = $resultat->fetchAll();
+	require('./views/home.phtml');
 ?>
